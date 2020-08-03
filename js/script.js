3823fd799fd63e6e388e50bf4708baa18963abff
manish kushwaha <tzmanish97@gmail.com>
2020-08-04 00:29:11 +0530

    added icons and scss cleanup

--- js/script.js
+++ js/script.js
@@ -1,41 +1,39 @@
 "use strict"
 
-if(window.CSS && CSS.supports("color", "var(--background)")){    //if color mode functionality is supported
-    const toggleButton = document.getElementById("color-mode-toggle-button");
-    toggleButton.style.display = "inline-block";
-    if(      //check os default and previous preference
-        localStorage.getItem("color-mode") == "dark" || 
-        (!localStorage.getItem("color-mode") && window.matchMedia('(prefers-color-scheme: dark').matches)
-        ) {
+function isDark(){
+    return (
+        localStorage.getItem("color-mode") == "dark"//check preference
+        || (
+            !localStorage.getItem("color-mode")     //no preference set
+            && window.matchMedia('(prefers-color-scheme: dark)').matches //check os default
+        )
+    );
+}
+
+if(window.CSS && CSS.supports("color", "var(--bg)")){   //if mode toggle supported
+    if(isDark()) {
         document.documentElement.setAttribute("color-mode", "dark");
-    } else document.documentElement.setAttribute("color-mode", "light");
+        document.getElementById("icon-dark").style.display = "flex";
+    } 
+    else {
+        document.documentElement.setAttribute("color-mode", "light");
+        document.getElementById("icon-light").style.display = "flex";
+    }
 }
 
 function toggleColorMode(){
     const mode = document.documentElement.getAttribute("color-mode");
+
     if(mode == "light"){
         document.documentElement.setAttribute("color-mode", "dark");
         localStorage.setItem("color-mode", "dark");
-    } else {
+        document.getElementById("icon-dark").style.display = "flex";
+        document.getElementById("icon-light").style.display = "none";
+    } 
+    else {
         document.documentElement.setAttribute("color-mode", "light");
-        localStorage.setItem("color-mode", "light")
+        localStorage.setItem("color-mode", "light");
+        document.getElementById("icon-light").style.display = "flex";
+        document.getElementById("icon-dark").style.display = "none";
     }
 }
-
-window.onload = function(){
-    const sections = document.querySelectorAll(".section-heading");
-    const container = document.getElementById("goto-section");
-    for(let i = 0; i < sections.length; i++){
-        const section = sections[i];
-        const id = "section"+(i+1);
-        section.setAttribute("id", id);
-        let button = document.createElement("button");
-        button.innerHTML = section.textContent[0];
-        button.onclick = function (){
-            const hashVal = '#'+id;
-            document.querySelector(hashVal).scrollIntoView({behavior: 'smooth'});
-            history.pushState(null, null, hashVal);
-        };
-        container.appendChild(button);
-    }
-};
