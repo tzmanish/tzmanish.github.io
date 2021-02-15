f27e9df12868282c841d793b5a62e84d0b308fd2
manish kushwaha <tzmanish97@gmail.com>
2021-02-16 00:24:35 +0530

    Fixed color, transitions, spelling and hyphenate

--- js/script.js
+++ js/script.js
@@ -1,48 +1,60 @@
 "use strict"
 
-function isDarkPreferred(){
-    return (
-        localStorage.getItem("color-mode") == "dark"//check preference
-        || (
-            !localStorage.getItem("color-mode")     //no preference set
-            && window.matchMedia('(prefers-color-scheme: dark)').matches //check os default
-        )
-    );
+class ThemeManager{
+    constructor(){
+        // return if mode toggle not supported
+        if(!window.CSS || !CSS.supports("color", "var(--bg)")) return;
+
+        document.getElementById("color-mode-toggle-button").classList.remove("hidden");
+        this.iconDark = document.getElementById("icon-dark");
+        this.iconLight = document.getElementById("icon-light");
+
+        if(this.isDarkPreferred()) {
+            document.documentElement.setAttribute("color-mode", "dark");
+            this.setColorModeIcon("dark");
+        } else {
+            document.documentElement.setAttribute("color-mode", "light");
+            this.setColorModeIcon("light");
+        }
+    }
+
+    isDarkPreferred(){
+        return (
+            localStorage.getItem("color-mode") == "dark"//check preference
+            || (
+                !localStorage.getItem("color-mode")     //no preference set
+                && window.matchMedia('(prefers-color-scheme: dark)').matches //check os default
+            )
+        );
+    }
+
+    setColorModeIcon(mode){
+        if(mode == "dark"){
+            console.log("to dark");
+            this.iconDark.style.display = "none";
+            this.iconLight.style.display = "flex";
+        }
+        else{
+            console.log("to light");
+            this.iconLight.style.display = "none";
+            this.iconDark.style.display = "flex";
+        }
+    }
+
+    toggleColorMode(){
+        const mode = document.documentElement.getAttribute("color-mode");
+    
+        if(mode == "light"){
+            document.documentElement.setAttribute("color-mode", "dark");
+            localStorage.setItem("color-mode", "dark");
+            this.setColorModeIcon("dark");
+        } 
+        else {
+            document.documentElement.setAttribute("color-mode", "light");
+            localStorage.setItem("color-mode", "light");
+            this.setColorModeIcon("light")
+        }
+    }
 }
 
-function setColorModeIcon(mode){
-    if(mode == "dark"){
-        document.getElementById("icon-light").style.display = "flex";
-        document.getElementById("icon-dark").style.display = "none";
-    }
-    else{
-        document.getElementById("icon-dark").style.display = "flex";
-        document.getElementById("icon-light").style.display = "none";
-    }
-}
-
-if(window.CSS && CSS.supports("color", "var(--bg)")){   //if mode toggle supported
-    if(isDarkPreferred()) {
-        document.documentElement.setAttribute("color-mode", "dark");
-        setColorModeIcon("dark");
-    } 
-    else {
-        document.documentElement.setAttribute("color-mode", "light");
-        setColorModeIcon("light")
-    }
-}
-
-function toggleColorMode(){
-    const mode = document.documentElement.getAttribute("color-mode");
-
-    if(mode == "light"){
-        document.documentElement.setAttribute("color-mode", "dark");
-        localStorage.setItem("color-mode", "dark");
-        setColorModeIcon("dark");
-    } 
-    else {
-        document.documentElement.setAttribute("color-mode", "light");
-        localStorage.setItem("color-mode", "light");
-        setColorModeIcon("light")
-    }
-}
+const themeManager = new ThemeManager();
