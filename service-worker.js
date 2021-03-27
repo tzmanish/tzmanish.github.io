64da3a8d7f3c51175467b1ab3b11e16d93ecbad6
manish kushwaha <tzmanish97@gmail.com>
2021-03-28 00:29:42 +0530

    service-worker reload bug

--- service-worker.js
+++ service-worker.js
@@ -8,9 +8,12 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open(CACHE_NAME);
-      // Setting {cache: 'reload'} in the new request will ensure that the
-      // response isn't fulfilled from the HTTP cache; i.e., it will be from
-      // the network.
+      await cache.addAll(
+        [
+          'media/icons/favicon-32x32.png',
+          'media/icons/favicon-16x16.png'
+        ]
+      );
       await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
     })()
   );
