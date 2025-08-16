3ad4d6178e005a157cf77caebedbcca9ccb3039b
Manish Kushwaha <tzmanish97@gmail.com>
2025-08-16 12:51:25 +0530

    Enhance logo styling with theme-aware colors and improve SVG structure

--- app/page.tsx
+++ app/page.tsx
@@ -29,21 +29,22 @@ export default function Portfolio() {
                 <div className="w-20 h-20 hover:scale-110 transition-transform duration-300">
                   <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="80" height="80" className="w-full h-full group" >
                     <defs>
-                      <path id="square" d="M0.5 0 A0.5 0.5 0 0 0 0 0.5 L0 6.5 A0.5 0.5 0 0 0 0.5 7 L6.5 7 A0.5 0.5 0 0 0 7 6.5 L7 0.5 A0.5 0.5 0 0 0 6.5 0 L0.5 0 z" />
-                      <path id="arc-tl" d="M6.5 0 A6.5 6.5 0 0 0 0 6.5 L0 6.5 A0.5 0.5 0 0 0 0.5 7 L6.5 7 A0.5 0.5 0 0 0 7 6.5 L7 0.5 A0.5 0.5 0 0 0 6.5 0 L6.5 0 z" />
-                      <path id="arc-tr" d="M0.5 0 A0.5 0.5 0 0 0 0 0.5 L0 6.5 A0.5 0.5 0 0 0 0.5 7 L6.5 7 A0.5 0.5 0 0 0 7 6.5 L7 6.5 A6.5 6.5 0 0 0 0.5 0 L0.5 0 z" />
-                      <path id="arc-br" d="M0.5 0 A0.5 0.5 0 0 0 0 0.5 L0 6.5 A0.5 0.5 0 0 0 0.5 7 L0.5 7 A6.5 6.5 0 0 0 7 0.5 L7 0.5 A0.5 0.5 0 0 0 6.5 0 L0.5 0 z" />
+                      <rect id="square" x="0" y="0" width="7" height="7" rx="0.5" ry="0.5"/>
+                      <path id="arc-tl" d="M6.5 0 A6.5 6.5 0 0 0 0 6.5 L0 6.5 A0.5 0.5 0 0 0 0.5 7 L6.5 7 A0.5 0.5 0 0 0 7 6.5 L7 0.5 A0.5 0.5 0 0 0 6.5 0 L6.5 0 z"/>
+                      <path id="arc-tr" d="M0.5 0 A0.5 0.5 0 0 0 0 0.5 L0 6.5 A0.5 0.5 0 0 0 0.5 7 L6.5 7 A0.5 0.5 0 0 0 7 6.5 L7 6.5 A6.5 6.5 0 0 0 0.5 0 L0.5 0 z"/>
+                      <path id="arc-br" d="M0.5 0 A0.5 0.5 0 0 0 0 0.5 L0 6.5 A0.5 0.5 0 0 0 0.5 7 L0.5 7 A6.5 6.5 0 0 0 7 0.5 L7 0.5 A0.5 0.5 0 0 0 6.5 0 L0.5 0 z"/>
                     </defs>
 
-                    <use href="#arc-tr" className="logo-theme" transform="translate(1,1)" />
-                    <use href="#arc-tl" className="logo-theme" transform="translate(8.5,1)" />
-                    <rect className="logo-theme logo-filler" x={16} y={1} width={7} height={7} rx={0.5} ry={0.5} />
-                    <use href="#square" className="logo-theme" transform="translate(1,8.5)" />
-                    <use href="#square" className="logo-theme" transform="translate(8.5,8.5)" />
-                    <use href="#arc-br" className="logo-theme" transform="translate(16,8.5)" />
-                    <rect className="logo-theme logo-filler" x={1} y={16} width={7} height={7} rx={0.5} ry={0.5} />
-                    <use href="#square" className="logo-theme" transform="translate(8.5,16)" />
-                    <use href="#arc-tr" className="logo-theme" transform="translate(16,16)" />
+                    <use href="#arc-tr" className="fixed" transform="translate(1,1)"/>
+                    <use href="#arc-tl" className="fixed" transform="translate(8.5,1)"/>
+                    <use href="#circle" className="filler" transform="translate(16,1)"/>
+                    <rect className="filler" x="16" y="1" width="7" height="7" rx="0.5" ry="0.5"/>
+                    <use href="#square" className="fixed" transform="translate(1,8.5)"/>
+                    <use href="#square" className="fixed" transform="translate(8.5,8.5)"/>
+                    <use href="#arc-br" className="fixed" transform="translate(16,8.5)"/>
+                    <rect className="filler" x="1" y="16" width="7" height="7" rx="0.5" ry="0.5"/>
+                    <use href="#square" className="fixed" transform="translate(8.5,16)"/>
+                    <use href="#arc-tr" className="fixed" transform="translate(16,16)"/>
                   </svg>
                 </div>
               </div>
